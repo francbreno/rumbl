b4ed3ab8c482a7cb85e53945de8a9e1a12f42f7b
Breno <francbreno@gmail.com>
2018-11-01 17:08:27 -0300

    Change Accounts to use Ecto Repo

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -4,31 +4,42 @@ defmodule Rumbl.Accounts do
   """
 
   alias Rumbl.Accounts.User
+  alias Rumbl.Repo
 
   @doc """
   Returns a list of all users in the system.
   """
   def list_users do
-    [
-      %User{id: "1", name: "José", username: "josevalim"},
-      %User{id: "2", name: "Bruce", username: "redrapids"},
-      %User{id: "3", name: "Chris", username: "chrismccord"}
-    ]
+    Repo.all(User)
   end
 
   @doc """
   Returns a user with the provided **id**.
   """
   def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+    Repo.get(User, id)
+  end
+
+  @doc """
+  Returns a user with the provided **id**.
+
+  Raises an Ecto.NotFoundError if user does not exist.
+  """
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   @doc """
   Returns a user with the provided filter **params**.
   """
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by(User, params)
+  end
+
+  @doc """
+  Creates a new user.
+  """
+  def change_user(%User{} = user) do
+    User.changetset(user, %{})
   end
 end
